7bf938b67216add156f9c9bcd70212671ee5b925
SinaRiyahi <sina.riyahi76@gmail.com>
2023-03-02 23:52:25 +0330

    image

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -75,7 +75,7 @@ function LoginBox() {
 
       <div className={classes.keyPic}>
         <img
-          src={require("../../assets/images/digikala.svg").default}
+          src={require("../../assets/images/digikala10.svg").default}
           alt="illustrator key"
           loading="lazy"
         />
